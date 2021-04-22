ff41ecdb6cddc6a0eef809f6efee42e936258080
Adam Siyala <69278320+AdamSiyala@users.noreply.github.com>
2021-04-21 18:41:32 -0700

    mongoose

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
+const database = include('databaseConnection');
+const User = include('models/user');
+const Pet = include('models/pet');
+const Joi = require("joi");
 
 router.get('/', async (req, res) => {
 	console.log("page hit");
@@ -13,4 +17,39 @@ router.get('/', async (req, res) => {
 	}
 });
 
+router.get("/populateData", async (req, res) => {
+	console.log("populate Data");
+	try {
+	let pet1 = new Pet({
+	name: "Fido"
+	});
+	let pet2 = new Pet({
+	name: "Rex"
+	});
+	await pet1.save();
+	//pet1.id contains the newly created pet's id
+	console.log(pet1.id);
+	await pet2.save();
+	//pet2.id contains the newly created pet's id
+	console.log(pet2.id);
+	let user = new User({
+	first_name: "Me",
+	last_name: "Awesome",
+	email: "a@b.ca",
+	password_hash: "thisisnotreallyahash",
+	password_salt: "notagreatsalt",
+	pets: [pet1.id, pet2.id]
+	}
+	);
+	await user.save();
+	//user.id contains the newly created user's id
+	console.log(user.id);
+	res.redirect("/");
+	}
+	catch(ex) {
+	res.render('error', {message: 'Error'});
+	console.log("Error");
+	console.log(ex);
+	}
+	});
 module.exports = router;
